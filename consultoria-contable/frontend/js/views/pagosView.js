a438283a11ec1b9819be9b889c3e4613c3092b44
VictoriaPerez <yumip5719@gmail.com>
2026-05-08 12:56:53 -0600

    CSS de pagos.html

---
+++ consultoria-contable/frontend/js/views/pagosView.js
@@ -0,0 +1,6 @@
+const Sub_HistorialPagos = document.getElementById("HistorialPagos");
+const Sub_PagosFaltantes = document.getElementById("pagos-faltantes");
+
+function CambiarSub(sub_apartado){
+    
+}
