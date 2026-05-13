d7da18cbdccc6ee613b34bcb92af8e24a2e8f990
Nieto2020 <nietoaguilar1@outlook.com>
2026-05-13 11:22:20 -0600

    Implement database connection in conexion.php

--- consultoria-contable/backend/config/conexion.php
+++ consultoria-contable/backend/config/conexion.php
@@ -1,1 +1,14 @@
-#aqui va la conexion a la base de datos
+<?php
+$host = "localhost";
+$db_name = "portalcliente";
+$username = "root";
+$password = "";
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->exec("set names utf8");
+} catch(PDOException $exception) {
+    echo "Error de conexión: " . $exception->getMessage();
+}
+?>
