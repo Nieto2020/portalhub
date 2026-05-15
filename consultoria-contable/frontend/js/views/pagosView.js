ce763fad0679c9ea72baa497fa079bfb8c04c5b7
yUMIP04 <yumip5719@gmail.com>
2026-05-15 09:59:14 -0600

    Apartado de Pagos listo

--- consultoria-contable/frontend/js/views/pagosView.js
+++ consultoria-contable/frontend/js/views/pagosView.js
@@ -29,61 +29,116 @@ Tab_PagosFaltantes.onclick = () =>{
     Tab_HistorialPagos.classList.remove('active');
 }
 
-/*CARGA DE DATOS EN LAS TABLAS */
-
+/* CARGA DE DATOS EN LAS TABLAS */
 const TablaHistorial = document.getElementById("HistorialPagos-Consultancy");
 const TablaFaltantes = document.getElementById("pagosFaltantes-Consultancy");
-function CargarTablasPagos(){
 
+function CargarTablasPagos() {
     const DatosSimulados = [
-
         {
-        tipo: "IVA",
-        fecha: "30/12/2025",
-        fecha_limite : "12/12/2026",
-        cantidad :"$3200",
-        estado : "Aprobado"
-    },
+            tipo: "IVA",
+            fecha: "30/12/2025",
+            fecha_limite: "12/12/2026",
+            cantidad: "$3200",
+            estado: "Aprobado"
+        },
+        {
+            tipo: "ISR",
+            fecha: "15/01/2026",
+            fecha_limite: "11/12/2026",
+            cantidad: "$1500",
+            estado: "Pendiente"
+        }
+    ];
 
-    {
-        tipo: "ISR",
-        fecha: "15/01/2026",
-         fecha_limite : "11/12/2026",
-        cantidad: "$1500",
-        estado: "Pendiente"
-    }
-];
-
-DatosSimulados.forEach(pago =>{
-    const fila_pago = document.createElement("tr");
-    const fila_faltante = document.createElement("tr");
-    let claseExtra = "";
-
-    if (pago.estado === "Aprobado") {
-        claseExtra = "aprobado"; 
-    } else if (pago.estado === "Pendiente") {
-        claseExtra = "pendiente";
-    } else if (pago.estado === "Vencido") {
-        claseExtra = "vencido";
-    }
-
-    fila_pago.innerHTML = `<td>${pago.tipo}</td>
-                            <td>${pago.fecha}</td>
-                            <td>${pago.cantidad}</td>
-                            <td><span class="status ${claseExtra}">${pago.estado}</span></td>
-                            <td><i class="bx bx-eye" style="color: white;"></i></td>`
-
-                            fila_faltante.innerHTML = `
-                             <td>${pago.tipo}</td>
-                            <td>${pago.fecha_limite}</td>
-                            <td><span class="status ${claseExtra}">${pago.estado}</span></td>
-                            <td> <i class="bx bx-alert-circle" /></i> 
-                                 <i class="bx bx-arrow-to-bottom-stroke" /></i> 
-                            </td>
-                            `;
-                            TablaHistorial.appendChild(fila_pago);
-                            TablaFaltantes.appendChild(fila_faltante);
-});
+    DatosSimulados.forEach(pago => {
+        let claseExtra = pago.estado.toLowerCase();
+        
+        
+        if (pago.estado === "Aprobado") {
+            const fila_pago = document.createElement("tr");
+            fila_pago.innerHTML = `
+                <td>${pago.tipo}</td>
+                <td>${pago.fecha}</td>
+                <td>${pago.cantidad}</td>
+                <td><span class="status ${claseExtra}">${pago.estado}</span></td>
+                <td><i class="bx bx-eye" style="color: white;"></i></td>
+            `;
+            TablaHistorial.appendChild(fila_pago);
+        } 
+        
+       
+        else if (pago.estado === "Pendiente" || pago.estado === "Vencido") {
+            const fila_faltante = document.createElement("tr");
+            fila_faltante.innerHTML = `
+                <td>${pago.tipo}</td>
+                <td>${pago.fecha_limite}</td>
+                <td><span class="status ${claseExtra}">${pago.estado}</span></td>
+                <td>
+                    <i class="bx bx-alert-circle"></i> 
+                    <i class="bx bx-arrow-to-bottom-stroke btn-subir-modal"></i> 
+                </td>
+            `;
+            TablaFaltantes.appendChild(fila_faltante);
+        }
+    });
 }
 
 CargarTablasPagos();
+
+/*MODAL PARA COMPROBANTE */
+
+const modalOverlay = document.getElementById("modal-container");
+
+function abrirModal(){
+    modalOverlay.style.display = "flex";
+}
+
+function cerrarModal(){
+    modalOverlay.style.display = "none";
+}
+
+TablaFaltantes.addEventListener("click", (e) =>{
+
+    if(e.target.classList.contains("btn-subir-modal")) {
+        abrirModal();
+    }
+});
+
+document.getElementById("btn-cancelar").onclick = cerrarModal;
+
+const btnSeleccionar = document.getElementById("btn-seleccionar");
+const inputOculto = document.getElementById("comprobante");
+const nombre_Archivo = document.getElementById("nombre-archivo");
+const btn_subir = document.getElementById("btn-subir");
+
+btnSeleccionar.onclick = () => {
+    inputOculto.click(); 
+};
+
+
+inputOculto.onchange = () => {
+    if (inputOculto.files.length > 0) {
+
+        nombre_Archivo.textContent = inputOculto.files[0].name;
+        console.log("Archivo seleccionado: " + inputOculto.files[0].name);
+        
+    }
+};
+
+btn_subir.onclick = () => {
+    if (inputOculto.files.length === 0) {
+        alert("Elige el comprobante para subirlo");
+    } else {
+        const archivo = inputOculto.files[0];
+        console.log("Subiendo " + archivo.name);
+        
+        alert(" ¡Comprobante subido con éxito! ");
+
+       
+        inputOculto.value = ""; 
+        nombre_Archivo.textContent = "No se ha seleccionado archivo";
+
+        cerrarModal();
+    }
+};
